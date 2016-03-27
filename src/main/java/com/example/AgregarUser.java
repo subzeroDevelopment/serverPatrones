525c1635b5ac5049f232ef3058b8239a03c2af06
Oribe Abad <subcero000@gmail.com>
2016-03-26 23:16:34 -0600

    probandoPSQL #3

--- src/main/java/com/example/AgregarUser.java
+++ src/main/java/com/example/AgregarUser.java
@@ -6,6 +6,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import java.sql.*;
 
 @Path("/addUser")
 public class AgregarUser{
