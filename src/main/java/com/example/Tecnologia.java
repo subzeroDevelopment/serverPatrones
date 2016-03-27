49938165a1c9c525b315a3f153d7fe8b3cbcad19
Oribe Abad <subcero000@gmail.com>
2016-03-26 21:01:24 -0600

    add tec.xml

---
+++ src/main/java/com/example/Tecnologia.java
@@ -0,0 +1,65 @@
+package com.example;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at \"myresource" path)
+ */
+@Path("getXML/Noticias")
+public class Noticias{
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces("text/xml")
+    public String getIt() {
+        return leer();
+    }
+
+
+    private String leer(){
+      File archivo =null;
+        FileReader fr = null;
+        BufferedReader br = null;
+        String texto="";
+        try {
+           // Apertura del fichero y creacion de BufferedReader para poder
+           // hacer una lectura comoda (disponer del metodo readLine()).
+           archivo = new File ("tec.xml");
+           fr = new FileReader (archivo);
+           br = new BufferedReader(fr);
+
+           // Lectura del fichero
+           String linea;
+           while((linea=br.readLine())!=null)
+              texto=texto+linea+"\n";
+              //System.out.println(linea);
+        }
+        catch(Exception e){
+           e.printStackTrace();
+        }finally{
+           // En el finally cerramos el fichero, para asegurarnos
+           // que se cierra tanto si todo va bien como si salta
+           // una excepcion.
+           try{
+              if( null != fr ){
+                 fr.close();
+              }
+           }catch (Exception e2){
+              e2.printStackTrace();
+           }
+        }
+        return texto;
+}
+}
